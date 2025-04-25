f511928ada51f7df6674f2689256b1db1ae3c8cc
petrazabickova <petra.zabickova@mzk.cz>
2025-04-25 14:54:31 +0200

    Nakresli zmrzlinu, sněhuláka a mašinku

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -10,10 +10,76 @@ public class HlavniProgram {
     }
 
     public void start() {
-        //TODO Tady bude kód pro kreslení želví grafiky.
+
+        zofka.turnRight(90);
+        posunDoprava(300);
+        zofka.turnLeft(90);
         nakresliSnehulaka();
+
+        posunDoleva(400);
+        nakresliMasinku();
+
+        zofka.turnRight(120);
+        posunDoleva(200);
+
+        zofka.turnLeft(60);
+        nakresliZmrzlinu();
     }
 
+
+    public void nakresliMasinku() {
+        // Rozměry
+        double sirkaSpodnihoDilu = 230;
+        double vyskaSpodnihoDilu = 70;
+
+        double sirkaHornihoDilu = 70;
+        double vyskaHornihoDilu = 150;
+
+        double prumerKoleckoMale = 40;
+        double prumerKoleckoVelke = 70;
+
+        // 1. Spodní obdélník (spodek mašinky)
+        nakresliObdelnik(sirkaSpodnihoDilu, vyskaSpodnihoDilu);
+
+        // 2. Horní obdélník (kabina)
+        nakresliObdelnik(sirkaHornihoDilu, vyskaHornihoDilu);
+
+        // 3. Kolečka
+        zofka.turnRight(90);
+        posunDoleva(70);
+        nakresliKruhZPrumeru(prumerKoleckoVelke, 30);
+
+        // První malé kolečko
+        posunDoleva(80);
+        nakresliKruhZPrumeru(prumerKoleckoMale, 30);
+
+        // Druhé malé kolečko
+        posunDoleva(prumerKoleckoMale + 40);
+        nakresliKruhZPrumeru(prumerKoleckoMale, 30);
+
+        // 4. Nárazník – trojúhelník
+        zofka.turnLeft(60);
+        nakresliRovnostrannyTrojuhelnik(70);
+    }
+
+
+    public void nakresliZmrzlinu() {
+        int pocetStran = 30;
+        double prumerKopecek = 90;
+        double delkaKornoutu = 90;
+
+        zofka.turnRight(90);
+        nakresliRovnostrannyTrojuhelnik(delkaKornoutu);
+        zofka.turnLeft(90);
+        nakresliKruhZPrumeru(prumerKopecek, pocetStran);
+    }
+
+    public void nakresliRovnostrannyTrojuhelnik(double delkaStrany) {
+        for (int i = 0; i < 3; i++) {
+            zofka.move(delkaStrany);
+            zofka.turnRight(120);
+        }
+    }
 
     public void nakresliSnehulaka() {
         int pocetStran = 30;
@@ -47,6 +113,7 @@ public class HlavniProgram {
         zofka.turnLeft(98);
         posunDoprava(prumerTelo + prumerRuce);
         nakresliKruhZPrumeru(prumerRuce, pocetStran);
+        zofka.turnLeft(172);
     }
 
     public void nakresliKruhZPrumeru(double prumer, double pocetStran) {
@@ -57,6 +124,15 @@ public class HlavniProgram {
         for (int i = 0; i < pocetStran; i++) {
             zofka.move(delkaStrany);
             zofka.turnRight(uhel);
+        }
+    }
+
+    public void nakresliObdelnik(double sirka, double vyska) {
+        for (int i = 0; i < 2; i++) {
+            zofka.move(sirka);
+            zofka.turnRight(90);
+            zofka.move(vyska);
+            zofka.turnRight(90);
         }
     }
 
@@ -76,6 +152,13 @@ public class HlavniProgram {
         zofka.penDown();
     }
 
+    public void posunDoleva(double vzdalenost) {
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(vzdalenost);
+        zofka.penDown();
+        zofka.turnRight(90);
+    }
 }
 
 
